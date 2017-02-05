d4141dd58cca5f0abc3d6fbf7c69699cee4c44c8
Joel Caprani <joelcaprani@Joels-MacBook-Air.local>
2017-02-05 14:51:20 -0500

    create locations added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170205190511) do
+ActiveRecord::Schema.define(version: 20170205194852) do
+
+  create_table "locations", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "city"
+    t.string "country"
+  end
 
   create_table "parts", force: :cascade do |t|
     t.string  "name"
